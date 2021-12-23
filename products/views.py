886ca61d62d330a3d812fdf84ed1e6957f8081db
jiin-coder <fpahstiq123@naver.com>
2021-12-23 12:12:37 +0900

    상세화면에 상품 옵션 추가

--- products/views.py
+++ products/views.py
@@ -3,17 +3,19 @@ from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Product
 
 def list(request: HttpRequest): # 상품 목록
-    product_list = Product.objects.order_by('-reg_date')
+    products = Product.objects.order_by('-reg_date')
 
     return render(request, 'products/product_list.html', {
-      "product_list": product_list
+      "products": products
     })
 
 
 def detail(request: HttpRequest, product_id): # 상세 화면
     product = get_object_or_404(Product, id=product_id)
 
+    product_reals = product.product_reals.order_by('option_1_display_name')
     return render(request, "products/product_detail.html", {
-        "product": product
+        "product": product,
+        "product_reals": product_reals,
     })
 
