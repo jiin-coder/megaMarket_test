b307408daa3ad1764c7707bd0228899952fbb831
jiin-coder <fpahstiq123@naver.com>
2021-12-24 12:40:27 +0900

    상품리스트 호버, 내비게이션바, 상세a,메인 화면 디자인

--- base/urls.py
+++ base/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 APP_NAME = ''
 
@@ -25,6 +26,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
     path('qna/', include('questions.urls')),
+    path('', views.index),
 ]
 
 
