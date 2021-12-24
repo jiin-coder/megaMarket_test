40e11ff5b2111ae120470bf99dafdebbae774d0f
jiin-coder <fpahstiq123@naver.com>
2021-12-24 13:27:00 +0900

    로그인한 사람만 질문폼 노출

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
-    path('signin/', views.signin),
-    path('signup/', views.signup),
-    path('signout/', views.signout),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('signout/', views.signout, name='signout'),
 ]
