7e700020ad1bf7097e977cdbd1218c3b0d1ac622
jiin-coder <fpahstiq123@naver.com>
2022-01-03 12:22:20 +0900

    카카오 로그인 테스트

--- accounts/views.py
+++ accounts/views.py
@@ -4,17 +4,57 @@ from django.contrib.auth.views import (
     logout_then_login, LoginView,
 )
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.sites import requests
 from django.db.models import QuerySet
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse
-from lazy_string import LazyString
 
 from .decorators import logout_required
 from .forms import SignupForm, FindUsernameForm
 from .models import User
+import requests
+from lazy_string import LazyString
+
+
+def kakao_login(request: HttpRequest):
+    client_id = "4fae2ada5774a4856d47dc3a9c270944"
+    REDIRECT_URI = "http://localhost:8000/accounts/signin/kakao/callback"
+    return redirect(
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={REDIRECT_URI}&response_type=code"
+    )
+
+
+def kakao_login_callback(request):
+    # (1)
+    code = request.GET.get("code")
+    client_id = "4fae2ada5774a4856d47dc3a9c270944"
+    REDIRECT_URI = "http://localhost:8000/accounts/signin/kakao/callback"
+
+    token_request = requests.get(
+        f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={REDIRECT_URI}&code={code}"
+    )
+
+    token_json = token_request.json()
+
+    error = token_json.get("error", None)
+    if error is not None:
+        raise KakaoException()
+
+    access_token = token_json.get("access_token")
+
+    profile_request = requests.get(
+        "https://kapi.kakao.com/v2/user/me",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    profile_json = profile_request.json()
+
+    id = profile_json.get("id")
+
+    return HttpResponse(id)
+
 
 class MyLoginView(SuccessMessageMixin, LoginView):
     template_name = "accounts/signin.html"
@@ -30,6 +70,7 @@ class MyLoginView(SuccessMessageMixin, LoginView):
         initial['username'] = self.request.GET.get('username', None)
 
         return initial
+
 
 
 @logout_required
