45c32f4230b40a7de72c3d6798f579abaa958a7b
unknown <jiin-coder>
2022-01-06 13:50:11 +0900

    이전 오류 재수정

--- base/settings/common.py
+++ base/settings/common.py
@@ -130,7 +130,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -143,7 +143,7 @@ AUTH_USER_MODEL = "accounts.User"
 STATICFILES_DIRS = [
     BASE_DIR / 'base/static',
 ]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / '/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
