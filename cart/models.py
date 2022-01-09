9e22345cd5ed101166ce99fb4d2e03ad65b16c83
jiin-coder <fpahstiq123@naver.com>
2022-01-10 03:16:00 +0900

    장바구니 품목 모델 생성

--- cart/models.py
+++ cart/models.py
@@ -6,8 +6,8 @@ from products.models import ProductReal
 
 
 class CartItem(models.Model):
-    reg_date = models.DateTimeField('등록날짜', auto_now_add=True)
-    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    reg_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product_real = models.ForeignKey(ProductReal, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField('개수')
+    quantity = models.PositiveSmallIntegerField()
